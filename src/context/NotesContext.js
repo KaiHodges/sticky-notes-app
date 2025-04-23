5f974375d802a7b59f6eaba01c0a7878429af056
tetli <182772561+tetli@users.noreply.github.com>
2025-04-23 13:14:03 +0200

    Delete React setup, switching to plain JavaScript

--- src/context/NotesContext.js
+++
@@ -1,4 +0,0 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { getNotes, saveNotes } from '../services/storageService';
-
-const NotesContext = createContext();
